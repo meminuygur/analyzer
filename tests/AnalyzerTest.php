ec9e16c668962e75f7e681ac337b9981e3058827
meminuygur <info@magefly.com>
2017-11-15 03:38:55 +0300

    ..

--- tests/AnalyzerTest.php
+++ tests/AnalyzerTest.php
@@ -11,10 +11,15 @@ class AnalyzerTest extends PHPUnit_Framework_TestCase
 {
 
 
-    public function testIsThereAnySyntaxError(){
-        $var = new Meminuygur\Analyzer\Analyzer();
-        $this->assertTrue(is_object($var));
-        unset($var);
+    public function testAfterCharsValid()
+    {
+
+        $text = 'football vs soccer';
+        $letter = 'f';
+        $analyzer = new Meminuygur\Analyzer\Analyzer();
+        $analyzer->setPhrase($text);
+        $this->assertTrue($analyzer->getAfterChars($letter) == array('o'));
+        //unset($var);
     }
 
 
