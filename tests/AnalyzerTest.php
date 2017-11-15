5636174b8eb4d88bf34c8efb6b2f9a8b9af5e1cc
meminuygur <info@magefly.com>
2017-11-15 03:58:13 +0300

    ...

--- tests/AnalyzerTest.php
+++ tests/AnalyzerTest.php
@@ -11,15 +11,59 @@ class AnalyzerTest extends PHPUnit_Framework_TestCase
 {
 
 
-    public function testAfterCharsValid()
+    public function testBeforeCharsValid()
     {
 
         $text = 'football vs soccer';
         $letter = 'f';
         $analyzer = new Meminuygur\Analyzer\Analyzer();
         $analyzer->setPhrase($text);
-        $this->assertTrue($analyzer->getAfterChars($letter) == array('o'));
-        //unset($var);
+        $this->assertTrue($analyzer->getBeforeChars($letter) == array('o'));
+
+    }
+
+
+    public function testAfterCharsIsEmpty()
+    {
+
+        $text = 'football vs soccer';
+        $letter = 'f';
+        $analyzer = new Meminuygur\Analyzer\Analyzer();
+        $analyzer->setPhrase($text);
+        $this->assertFalse($analyzer->getAfterChars($letter));
+
+    }
+
+    public function testAfterChars()
+    {
+
+        $text = 'football vs soccer';
+        $letter = 'o';
+        $analyzer = new Meminuygur\Analyzer\Analyzer();
+        $analyzer->setPhrase($text);
+        $this->assertTrue($analyzer->getAfterChars($letter) == ['f', 'o', 's']);
+
+    }
+
+    public function testMaxDistance()
+    {
+
+        $text = 'football vs soccer';
+        $letter = 'o';
+        $analyzer = new Meminuygur\Analyzer\Analyzer();
+        $analyzer->setPhrase($text);
+        $this->assertTrue($analyzer->getMaxDistance($letter) == 11);
+
+    }
+
+    public function testCountChar()
+    {
+
+        $text = 'football vs soccer';
+        $letter = 'o';
+        $analyzer = new Meminuygur\Analyzer\Analyzer();
+        $analyzer->setPhrase($text);
+        $this->assertTrue($analyzer->getCountValue($letter) == 3);
     }
 
 
